ad881d7031647d52243fa6c4bfc23d4cc0930442
Craig Whippo <craig.whippo@dickinsonstate.edu>
2019-01-02 14:57:20 -0700

    .js

---
+++ questions.js
@@ -0,0 +1,23 @@
+const questions = {
+  'Q01': {
+    'type': 'highlight',
+    'title': 'Motive',
+    'Question': 'Highlight the statements describing reasons why additional research is needed',
+    'anchored': true,
+    'repeatable': true,
+  },
+    'Q02': {
+    'type': 'highlight',
+    'title': 'Objective',
+    'Question': 'Highlight the statements describing what the authors wanted to know',
+    'anchored': true,
+    'repeatable': true,
+  },
+    },
+    'Q03': {
+    'type': 'highlight',
+    'title': 'General Approach',
+    'Question': 'Highlight the statements describing what the authors general approach',
+    'anchored': true,
+    'repeatable': true,
+}
